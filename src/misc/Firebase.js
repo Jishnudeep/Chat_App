01d321413cdde93b519710b8f4ca786b3a956d5f
Jishnudeep Bhattacharjee <bjishnudeep@gmail.com>
2021-08-15 21:55:16 +0530

    edited sign in page and added sign in with google

--- src/misc/Firebase.js
+++ src/misc/Firebase.js
@@ -1,8 +1,12 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: 'AIzaSyDFeui0TFOhyhetQI7vDTvipEoREB6ssIk',
   authDomain: 'chat-web-app-b041a.firebaseapp.com',
+  databaseURL:
+    'https://chat-web-app-b041a-default-rtdb.asia-southeast1.firebasedatabase.app/',
   projectId: 'chat-web-app-b041a',
   storageBucket: 'chat-web-app-b041a.appspot.com',
   messagingSenderId: '26636133793',
@@ -10,3 +14,6 @@ const config = {
 };
 
 const app = firebase.initializeApp(config);
+
+export const auth = app.auth();
+export const database = app.database();
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,51 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import { Container, Grid, Row, Panel, Col, Button, Icon, Alert } from 'rsuite';
+import { auth, database } from '../misc/Firebase';
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const signInWithProvider = async provider => {
+    try {
+      const { additionalUserInfo, user } = await auth.signInWithPopup(provider);
+
+      if (additionalUserInfo.isNewUser) {
+        await database.ref(`/profiles/${user.uid}`).set({
+          name: user.displayName,
+          createdAt: firebase.database.ServerValue.TIMESTAMP,
+        });
+      }
+
+      Alert.success('Signed In!', 4000);
+    } catch (err) {
+      Alert.error('err.message', 4000);
+    }
+  };
+
+  const onGoogleSignin = () => {
+    signInWithProvider(new firebase.auth.GoogleAuthProvider());
+  };
+
+  return (
+    <Container>
+      <Grid className="mt-page">
+        <Row>
+          <Col xs={24} md={12} mdOffset={6}>
+            <Panel>
+              <div className="text-center">
+                <h2>Welcome to Chattr!</h2>
+                <p>Progressive Chat Platform for neophytes!</p>
+              </div>
+              <div className="mt-3">
+                <Button block color="red" onClick={onGoogleSignin}>
+                  <Icon icon="google">Sign In With Google</Icon>
+                </Button>
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
+  );
 };
 
 export default SignIn;
