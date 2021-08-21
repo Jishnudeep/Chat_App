d93f9522ce771171c73a75afe1fc124d105b9a67
Jishnudeep Bhattacharjee <bjishnudeep@gmail.com>
2021-08-21 17:42:26 +0530

    created layout for chat page

---
+++ src/components/chat-window/messages/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Messages = () => {
+  return <div>awdwd</div>;
+};
+
+export default Messages;
--- src/pages/Home/Chat.js
+++ src/pages/Home/Chat.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { Loader } from 'rsuite';
+import { useParams } from 'react-router';
+import Top from '../../components/chat-window/top';
+import Bottom from '../../components/chat-window/bottom';
+import Messages from '../../components/chat-window/messages';
+import { useRooms } from '../../context/rooms.context';
 
 const Chat = () => {
-  return <div>Chart</div>;
+  const { chatId } = useParams();
+
+  const rooms = useRooms();
+
+  if (!rooms) {
+    return <Loader center vertical size="md" speed="slow" />;
+  }
+
+  const currRoom = rooms.find(room => room.id === chatId);
+  if (!currRoom) {
+    return <h6 className="text-center mt-page">Chat Id Not Found</h6>;
+  }
+
+  return (
+    <>
+      <div className="chat-top">
+        <Top />
+      </div>
+      <div className="chat-middle">
+        <Messages />
+      </div>
+      <div className="chat-bottom">
+        <Bottom />
+      </div>
+    </>
+  );
 };
 
 export default Chat;
